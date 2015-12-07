fdabaa4795ef8f88eb5c2879b2d2de1762e6c81a
Alexandre Pagé <alexandre.page.work@gmail.com>
2015-12-07 12:33:36 -0500

    Update PermisAllController.js

--- api/controllers/PermisAllController.js
+++ api/controllers/PermisAllController.js
@@ -8,26 +8,21 @@
 module.exports = {
 	
 	all: function (req, res) {
-	  console.log(req.params);
-	  if (req.params['WOR_NO']) {
-	    PermisView.query("SELECT * FROM Permis WHERE WOR_NO=" + req.params['WOR_NO'], function (err, p) {
-	      if (err) { return res.json(err) }
-	      res.json(p);
-	    })
-	  } else {
+
 	   	PermisView.query("SELECT TOP 300 * FROM Permis ORDER BY id DESC", function (err, p) {
   	    if (err) {
   	      return res.json(err);
   	    }
   	    res.json(p);
   	  }) 
-	  }
+	  
 
 	  /*
 		PermisAll.find().limit(300).then(function (permis) {
 			res.json(permis);
 		});
 		*/
+		
 	},
 
 };

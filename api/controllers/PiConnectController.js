9e521e028152abb23787182ccf4eba9d24121854
qsi-info <qsi-info@users.noreply.github.com>
2015-12-11 08:36:39 -0500

    Update PiConnectController.js

--- api/controllers/PiConnectController.js
+++ api/controllers/PiConnectController.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = {
+  
+  test: function (req, res) {
+    PiConnect.find().then(function (p) {
+      res.json(p);
+    })
+    .fail(function (err) {
+      console.log(err);
+      res.json(err);
+    })
+  }
+}

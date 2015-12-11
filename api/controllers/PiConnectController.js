9692caeab16a2b9a83ed541f6325f3a454e5744e
qsi-info <qsi-info@users.noreply.github.com>
2015-12-11 09:32:10 -0500

    Update PiConnectController.js

--- api/controllers/PiConnectController.js
+++ api/controllers/PiConnectController.js
@@ -1,7 +1,7 @@
 module.exports = {
   
   last: function (req, res) {
-    PiConnect.query("SELECT TOP 1 * FROM [dbo].[QSI_Values] ORDER BY CREATED_AT DESC ").exec(function (err, results) {
+    PiConnect.query("SELECT TOP 1 * FROM [dbo].[QSI_Values] ORDER BY CREATED_AT DESC ", function (err, results) {
       if (err) {
         return res.json(err);
       } else {
